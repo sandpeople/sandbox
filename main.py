338fb0bdfab09fb4d5a16fb705729478c33f110e
Jonas Thiem <jonasthiem@googlemaill.com>
2015-09-19 18:13:42 +0200

    Fixing indentation

--- main.py
+++ main.py
@@ -17,17 +17,18 @@ def get_depth():
     return img
 
 while run is True:
-       img = get_depth() #cv2.imread('kinect.png',0) #get_depth()
-       cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-       #print "gradient:%s img:%s cimg:%s" %(gradient.shape, img.shape, cimg.shape)
-       #print "gradient[10,100]: %s wert: %s" %(gradient[10,100], img[10,100])
-       for x in range(0,img.shape[0]):
-       	for y in range(0,img.shape[1]):
-       		pixel=255-img[x,y]
-       		pixel=((pixel*offset)+height)%255
-       		cimg[x,y]=gradient[10,pixel]
-       cv2.imshow('Depth', cimg)
-       #cv.ShowImage('Depth', img)
-       run=True
-       if cv.WaitKey(10) == 27:
-           sys.exit(0)
+    img = get_depth() #cv2.imread('kinect.png',0) #get_depth()
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+    #print "gradient:%s img:%s cimg:%s" %(gradient.shape, img.shape, cimg.shape)
+    #print "gradient[10,100]: %s wert: %s" %(gradient[10,100], img[10,100])
+    for x in range(0,img.shape[0]):
+        for y in range(0,img.shape[1]):
+            pixel=255-img[x,y]
+            pixel=((pixel*offset)+height)%255
+            cimg[x,y]=gradient[10,pixel]
+   cv2.imshow('Depth', cimg)
+   #cv.ShowImage('Depth', img)
+   run=True
+   if cv.WaitKey(10) == 27:
+       sys.exit(0)
+
