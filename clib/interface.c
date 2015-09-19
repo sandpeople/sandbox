37704f4e347af9df47d592d7b6be500a34c6ca56
Jonas Thiem <jonasthiem@googlemaill.com>
2015-09-19 20:34:12 +0200

    Clean up main code

--- clib/interface.c
+++ clib/interface.c
@@ -1,15 +1,18 @@
 
 #include <stdio.h>
+#include <stdint.h>
 
-void cfun(const void* depth_array, int xsize, int ysize,
-        void* pixelvalues) {
-    const double * indata = (double *) indatav;
-    double * outdata = (double *) outdatav;
+void run_simulation(const void *depth_array_v, int xsize, int ysize,
+        void *output_colors_v) {
+    const uint8_t *depth_array = (uint8_t*)depth_array_v;
+    uint8_t *output_colors = (uint8_t*)output_colors_v;
     int i;
-    print("got data.\n");
-    for (i = 0; i < rowcount * colcount; ++i) {
-        outdata[i] = indata[i] * 2;
+    for (i = 0; i < xsize * ysize; ++i) {
+        int offset = i * 3;
+        output_colors[offset] = depth_array[i] * 2;
+        output_colors[offset+1] = depth_array[i] * 1;
+        output_colors[offset+2] = 0;
     }
-    print("processing complete\n");
+    printf("processing complete\n");
 }
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import frame_convert
 import sys
+from clib_interface import call_clib_sim
 
 height=80
 offset=3.5
@@ -15,7 +16,7 @@ cv.NamedWindow('Depth')
 cv2.setWindowProperty("Depth", cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
 
 def get_depth():
-    """ This ufnction obtains the depth image from the kinect, if any is
+    """ This function obtains the depth image from the kinect, if any is
         connected.
     """
     img = frame_convert.pretty_depth(sync_get_depth()[0])
@@ -37,20 +38,14 @@ cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 while run is True:
     # take kinect image if we have one:
     img = get_image()
-    #print("Array type of incoming image: " + str(img.dtype))
-    #print("Array type of outcoming image: " + str(cimg.dtype)
 
+    # call C code for simulation:
+    call_clib_sim(img, cimg)
 
-    #print "gradient:%s img:%s cimg:%s" %(gradient.shape, img.shape, cimg.shape)
-    #print "gradient[10,100]: %s wert: %s" %(gradient[10,100], img[10,100])
-    for x in range(0, img.shape[0]):
-        for y in range(0, img.shape[1]):
-            pixel = 255 - img[x,y]
-            pixel = ((pixel*offset)+height)%255
-            cimg[x,y] = gradient[10, pixel]
+    # show resulting image:
     cv2.imshow('Depth', cimg)
-    #cv.ShowImage('Depth', img)
-    run=True
+   
+    # quit if escape is pressed: 
     if cv.WaitKey(10) == 27:
         sys.exit(0)
 
