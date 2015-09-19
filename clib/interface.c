e40e6fefe939aabcb4b6e414fdf34e8dd8aeea22
Jonas Thiem <jonasthiem@googlemaill.com>
2015-09-19 19:21:21 +0200

    Refactoring interface code

--- clib/interface.c
+++ clib/interface.c
@@ -1,15 +1,15 @@
 
 #include <stdio.h>
 
-void cfun(const void * indatav, int rowcount, int colcount, void * outdatav) {
-    //void cfun(const double * indata, int rowcount, int colcount, double * outdata) {
-    //    const double * indata = (double *) indatav;
-    //        double * outdata = (double *) outdatav;
-    //            int i;
-    //                puts("Here we go!");
-    //                    for (i = 0; i < rowcount * colcount; ++i) {
-    //                            outdata[i] = indata[i] * 2;
-    //                                }
-    //                                    puts("Done!");
-    //                                    }
-    //
+void cfun(const void* depth_array, int xsize, int ysize,
+        void* outdatav) {
+    const double * indata = (double *) indatav;
+    double * outdata = (double *) outdatav;
+    int i;
+    print("got data.\n");
+    for (i = 0; i < rowcount * colcount; ++i) {
+        outdata[i] = indata[i] * 2;
+    }
+    print("processing complete\n");
+}
+
